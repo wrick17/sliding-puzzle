32175ed4dc6c472c8bbd408b4628abee48275d2a
Pratyush Poddar <wrick17@gmail.com>
2016-07-16 16:08:27 +0530

    put in levels of complexity

--- script.js
+++ script.js
@@ -2,213 +2,272 @@
 
   console.log('Yo Buddy! Welcome to the sliding puzzle game');
 
-  // grid size of the puzzle
-  const size = 2;
+  let busyFlag = false;
 
-  const randomComplexity = 5;
-  const randomizationTimeout = 200;
+  let randomizeQueue, timeout;
 
-  // puzzle object
-  const puzzle = $('#puzzle');
+  function gameInit() {
 
-  // make the tiles index
-  const tiles = [], lastTileId = (size * size) - 1;
-  for (var i = 0; i <= lastTileId; i++) {
-    tiles.push(i);
-  }
+    $('.init-box').hide();
+    $('#reset').show();
 
-  let blankTileId = size * size - 1;
+    // get the user's difficulty level
+    const difficulty = Number($('#difficulty').val());
 
-  // get size of each tile
-  const tileDimension = (puzzle.innerHeight())/size;  // the 2 at the end is just for laziness purposes to cut off the bad styling :p
+    // grid size of the puzzle
+    const size = difficulty;
 
-  // create the tile elements
-  const tileElements = tiles.map(tileId => {
+    // set some complexity of the randomization
+    const randomComplexity = size * (2 + size);
+    const randomizationTimeout = 200 + 10;
 
-    // create an html object
-    const tile = $($.parseHTML('<div class="tile" data-id="' + tileId + '" id="tile' + tileId + '">' + tileId + '</div>'));
+    // set puzzle dimensions
+    const body = $('body');
+    const bodyHeight = body.innerHeight();
+    const bodyWidth = body.innerWidth();
 
-    // get tile position
-    const y = parseInt(tileId / size);
-    const x = tileId % size;
+    // set it to 60% of the browser window size
+    const puzzleDimension = Math.min(bodyHeight, bodyWidth) * 0.6;
 
-    tile.data('x', x);
-    tile.data('y', y);
+    // puzzle object
+    const puzzle = $('#puzzle');
 
-    // calculate positions
-    const left = x * tileDimension;
-    const top = y * tileDimension;
+    puzzle.css({ height: puzzleDimension, width: puzzleDimension });
 
-    // set the size of the tiles
-    tile.css({ height: tileDimension, width: tileDimension, top: top, left: left });
-
-    if (tileId === blankTileId) {
-      // set blank tile as blank, apparantly
-      tile.css({ background: 'white', 'z-index': 0 });
+    // make the tiles index
+    const tiles = [], lastTileId = (size * size) - 1;
+    for (var i = 0; i <= lastTileId; i++) {
+      tiles.push(i);
     }
 
-    return tile;
-  });
+    let blankTileId = size * size - 1;
 
-  // put them in the DOM
-  puzzle.append(tileElements);
+    // get size of each tile
+    const tileDimension = (puzzle.innerHeight())/size;  // the 2 at the end is just for laziness purposes to cut off the bad styling :p
 
-  function checkOrder() {
-    const tiles = $('.tile');
-    const faultArray = [];
-    $.each(tiles, function(idx, el) {
-      if ($(el).data('id') != $(el).attr('id').replace('tile', '')) faultArray.push(this);
+    // create the tile elements
+    const tileElements = tiles.map(tileId => {
+
+      // create an html object
+      const tile = $($.parseHTML('<div class="tile" data-id="' + tileId + '" id="tile' + tileId + '">' + tileId + '</div>'));
+
+      // get tile position
+      const y = parseInt(tileId / size);
+      const x = tileId % size;
+
+      tile.data('x', x);
+      tile.data('y', y);
+
+      // calculate positions
+      const left = x * tileDimension;
+      const top = y * tileDimension;
+
+      // set the size of the tiles
+      tile.css({ height: tileDimension, width: tileDimension, top: top, left: left });
+
+      if (tileId === blankTileId) {
+        // set blank tile as blank, apparantly
+        tile.css({ background: 'white', 'z-index': 0 });
+      }
+
+      return tile;
     });
-    if (faultArray.length === 0) setTimeout(() => {
-      alert('hurry you won!')
-    }, randomizationTimeout);
-  }
 
-  function swap(tileId, checkOrderFlag) {
-    // get the tile which has been clicked
-    const tile = $('#'+tileId);
+    // put them in the DOM
+    puzzle.append(tileElements);
 
-    // get tile's placement
-    const x = tile.data('x');
-    const y = tile.data('y');
+    function checkOrder() {
+      const tiles = $('.tile');
+      const faultArray = [];
+      $.each(tiles, function(idx, el) {
+        if ($(el).data('id') != $(el).attr('id').replace('tile', '')) faultArray.push(this);
+      });
+      if (faultArray.length === 0) setTimeout(() => {
+        alert('hurry you won!')
+      }, randomizationTimeout);
+    }
 
-    // get blankTile
-    const blankTile = $('#tile' + blankTileId);
+    function swap(tileId, checkOrderFlag) {
+      // get the tile which has been clicked
+      const tile = $('#'+tileId);
 
-    // get blank tile's placement
-    const blankTileX = blankTile.data('x');
-    const blankTileY = blankTile.data('y');
+      // get tile's placement
+      const x = tile.data('x');
+      const y = tile.data('y');
 
-    // console.log('old => ', blankTileX, blankTileY);
+      // get blankTile
+      const blankTile = $('#tile' + blankTileId);
 
-    // get tile's new positions
-    const newTileX = blankTileX;
-    const newTileY = blankTileY;
+      // get blank tile's placement
+      const blankTileX = blankTile.data('x');
+      const blankTileY = blankTile.data('y');
 
-    // get tile's new absolute positions
-    const newTileTop = newTileX * tileDimension;
-    const newTileLeft = newTileY * tileDimension;
+      // console.log('old => ', blankTileX, blankTileY);
 
-    // get blank tile's new positions
-    const newBlankTileX = x;
-    const newBlankTileY = y;
+      // get tile's new positions
+      const newTileX = blankTileX;
+      const newTileY = blankTileY;
 
-    // get blank tile's new absolute positions
-    const newBlankTileTop = newBlankTileX * tileDimension;
-    const newBlankTileLeft = newBlankTileY * tileDimension;
+      // get tile's new absolute positions
+      const newTileTop = newTileX * tileDimension;
+      const newTileLeft = newTileY * tileDimension;
 
-    // wait for the brower to get it's hands free
-    requestAnimationFrame(() => {
+      // get blank tile's new positions
+      const newBlankTileX = x;
+      const newBlankTileY = y;
 
-      // set positions for tile
-      tile.css({ left: newTileTop, top: newTileLeft });
-      tile.data('x', newTileX);
-      tile.data('y', newTileY);
-      tile.attr('id', 'tile' + blankTileId);
+      // get blank tile's new absolute positions
+      const newBlankTileTop = newBlankTileX * tileDimension;
+      const newBlankTileLeft = newBlankTileY * tileDimension;
 
-      // set positions for the blank tile
-      blankTile.css({ left: newBlankTileTop, top: newBlankTileLeft });
-      blankTile.data('x', newBlankTileX);
-      blankTile.data('y', newBlankTileY);
-      blankTile.attr('id', tileId);
+      // wait for the brower to get it's hands free
+      requestAnimationFrame(() => {
 
-      blankTileId = tileId.replace('tile', '');
+        // set positions for tile
+        tile.css({ left: newTileTop, top: newTileLeft });
+        tile.data('x', newTileX);
+        tile.data('y', newTileY);
+        tile.attr('id', 'tile' + blankTileId);
 
-      if (checkOrderFlag) checkOrder();
+        // set positions for the blank tile
+        blankTile.css({ left: newBlankTileTop, top: newBlankTileLeft });
+        blankTile.data('x', newBlankTileX);
+        blankTile.data('y', newBlankTileY);
+        blankTile.attr('id', tileId);
+
+        blankTileId = tileId.replace('tile', '');
+
+        if (checkOrderFlag) checkOrder();
+      })
+    }
+
+
+    $('#puzzle').on('click', '.tile', e => {
+
+      if (busyFlag) return;
+
+      // get the tile which has been clicked
+      const tileId = e.target.id;
+      const tile = $('#'+tileId);
+
+      // get tile's placement
+      const x = tile.data('x');
+      const y = tile.data('y');
+
+      // get blankTile
+      const blankTile = $('#tile' + blankTileId);
+
+      // get blank tile's placement
+      const blankTileX = blankTile.data('x');
+      const blankTileY = blankTile.data('y');
+
+      if (Math.abs(x - blankTileX) === 1 && Math.abs(y - blankTileY) === 0 || Math.abs(x - blankTileX) === 0 && Math.abs(y - blankTileY) === 1) {
+        swap(tileId, true);
+      }
+
     })
-  }
 
-
-  $('#puzzle').on('click', '.tile', e => {
-
-    // get the tile which has been clicked
-    const tileId = e.target.id;
-    const tile = $('#'+tileId);
-
-    // get tile's placement
-    const x = tile.data('x');
-    const y = tile.data('y');
-
-    // get blankTile
-    const blankTile = $('#tile' + blankTileId);
-
-    // get blank tile's placement
-    const blankTileX = blankTile.data('x');
-    const blankTileY = blankTile.data('y');
-
-    if (Math.abs(x - blankTileX) === 1 && Math.abs(y - blankTileY) === 0 || Math.abs(x - blankTileX) === 0 && Math.abs(y - blankTileY) === 1) {
-      swap(tileId, true);
+    function ArrNoDupe(a) {
+      var temp = {};
+      for (var i = 0; i < a.length; i++)
+      temp[a[i]] = true;
+      var r = [];
+      for (var k in temp)
+      r.push(k);
+      return r;
     }
 
-  })
+    function fetchNeighbours() {
+      const blankTile = $('#tile' + blankTileId);
 
-  function ArrNoDupe(a) {
-    var temp = {};
-    for (var i = 0; i < a.length; i++)
-    temp[a[i]] = true;
-    var r = [];
-    for (var k in temp)
-    r.push(k);
-    return r;
-  }
+      const x = blankTile.data('x');
+      const y = blankTile.data('y');
 
-  function fetchNeighbours() {
-    const blankTile = $('#tile' + blankTileId);
+      const neighbours = [];
 
-    const x = blankTile.data('x');
-    const y = blankTile.data('y');
-
-    const neighbours = [];
-
-    if ( (x > 0 && x <= (size - 1)) && (y > 0 && y <= (size - 1)) ) {
-      // right bottom
-      neighbours.push((y - 1) * size + x); // up
-      neighbours.push(y * size + (x - 1)); // left
+      if ( (x > 0 && x <= (size - 1)) && (y > 0 && y <= (size - 1)) ) {
+        // right bottom
+        neighbours.push((y - 1) * size + x); // up
+        neighbours.push(y * size + (x - 1)); // left
+      }
+      if ( (x >= 0 && x < (size - 1)) && (y >= 0 && y < (size - 1)) ) {
+        // left top
+        neighbours.push((y + 1) * size + x); // bottom
+        neighbours.push(y * size + (x + 1)); // right
+      }
+      if ( (x > 0 && x <= (size - 1)) && (y >= 0 && y < (size - 1)) ) {
+        // right top
+        neighbours.push(y * size + (x - 1)); // left
+        neighbours.push((y + 1) * size + x); // bottom
+      }
+      if ( (x >= 0 && x < (size - 1)) && (y > 0 && y <= (size - 1)) ) {
+        // left bottom
+        neighbours.push((y - 1) * size + x); // up
+        neighbours.push(y * size + (x + 1)) // right
+      }
+      return ArrNoDupe(neighbours);
     }
-    if ( (x >= 0 && x < (size - 1)) && (y >= 0 && y < (size - 1)) ) {
-      // left top
-      neighbours.push((y + 1) * size + x); // bottom
-      neighbours.push(y * size + (x + 1)); // right
+
+    function getRandomInt(array) {
+      const min = 0, max = (array.length - 1);
+      return array[Math.floor(Math.random() * (max - min + 1)) + min];
     }
-    if ( (x > 0 && x <= (size - 1)) && (y >= 0 && y < (size - 1)) ) {
-      // right top
-      neighbours.push(y * size + (x - 1)); // left
-      neighbours.push((y + 1) * size + x); // bottom
-    }
-    if ( (x >= 0 && x < (size - 1)) && (y > 0 && y <= (size - 1)) ) {
-      // left bottom
-      neighbours.push((y - 1) * size + x); // up
-      neighbours.push(y * size + (x + 1)) // right
-    }
-    return ArrNoDupe(neighbours);
-  }
-
-  function getRandomInt(array) {
-    const min = 0, max = (array.length - 1);
-    return array[Math.floor(Math.random() * (max - min + 1)) + min];
-  }
 
 
-  function randomize() {
+    function randomize() {
 
-    let oldNeighbour, temp;
-    for (var i = 0; i < randomComplexity; i++) {
-      setTimeout(() => {
+      // these two people are references of the last two moves so that they are not repeated
+      let oldNeighbour, temp;
+
+      // choose a random swappable neighbour for the selected number of times
+
+      busyFlag = true;
+
+      // set interval for the random swaps
+      randomizeQueue = setInterval(function() {
+
+        // find the neighbours
         let neighbours = fetchNeighbours();
         let swapWith = 'tile' + getRandomInt(neighbours);
+
+        // if it is repeating then choose again
         if (swapWith === oldNeighbour) {
           neighbours = fetchNeighbours();
           swapWith = 'tile' + getRandomInt(neighbours);
         }
+
+        // set the oldNeighbour to the current one
         oldNeighbour = temp;
         temp = swapWith;
+
+        // make the swap
         swap(swapWith);
-      }, (i * randomizationTimeout))
+
+      }, (randomizationTimeout + 10));
+
+      // stop the interval on time
+      timeout = setTimeout(() => {
+        clearInterval(randomizeQueue);
+        busyFlag = false;
+      }, ((randomizationTimeout + 10 ) * randomComplexity ));
     }
+
+    randomize();
+
   }
 
-  randomize();
+  function gameReset() {
 
-  $('.button').on('click', randomize);
+    clearInterval(randomizeQueue);
+    clearTimeout(timeout);
+    busyFlag = false;
+
+    $('#puzzle').html('');
+    $('.init-box').show();
+    $('#reset').hide();
+  }
+
+  $('#start').on('click', gameInit);
+  $('#reset').on('click', gameReset);
 
 })()
